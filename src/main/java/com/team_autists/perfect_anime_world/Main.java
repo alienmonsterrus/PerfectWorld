ade595f10a2cc561c359169592a8e9f954c1c424
Yaroslav <yaroslavyar@example.com>
2018-07-22 22:19:39 +0700

    Created base mod structure

--- src/main/java/com/team_autists/perfect_anime_world/Main.java
+++ src/main/java/com/team_autists/perfect_anime_world/Main.java
@@ -1,4 +1,29 @@
 package com.team_autists.perfect_anime_world;
 
+import com.team_autists.perfect_anime_world.proxy.CommonProxy;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.SidedProxy;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.fml.relauncher.SideOnly;
+
+@Mod(modid = Reference.MOD_ID, name = Reference.NAME, version = Reference.VERSION)
 public class Main {
+
+	@SidedProxy(clientSide = Reference.CLIENT, serverSide = Reference.COMMON)
+	private static CommonProxy proxy;
+
+	public void preInit(FMLPreInitializationEvent event) {
+		proxy.preInit(event);
+	}
+
+	public void init(FMLInitializationEvent event) {
+		proxy.init(event);
+	}
+
+	public void postInit(FMLPostInitializationEvent event) {
+		proxy.postInit(event);
+	}
 }
---
+++ src/main/java/com/team_autists/perfect_anime_world/Reference.java
@@ -0,0 +1,9 @@
+package com.team_autists.perfect_anime_world;
+
+public class Reference {
+	public static final String MOD_ID = "perfect_anime_world";
+	public static final String NAME = "Perfect Anime World";
+	public static final String VERSION = "1.0";
+	public static final String COMMON = "com.team_autists.perfect_anime_world.proxy.CommonProxy";
+	public static final String CLIENT = "com.team_autists.perfect_anime_world.proxy.ClientProxy";
+}
